4f96ca05737b9e171fe3dd48e48e69334be712fd
Edouard Butaye <edouard.butaye@cea.fr>
2025-02-12 14:24:01 +0100

    [fpi] new class Solid Particle

---
+++ src/Multiphase/fluid_particle_interaction/Solid_Particle.cpp
@@ -0,0 +1,93 @@
+/****************************************************************************
+* Copyright (c) 2025, CEA
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+* 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+* OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*****************************************************************************/
+
+#include <Solid_Particle.h>
+#include <Param.h>
+
+Implemente_instanciable_sans_constructeur(Solid_Particle,"Solid_Particle",Fluide_Incompressible);
+
+
+Solid_Particle::Solid_Particle()
+{}
+
+Sortie& Solid_Particle::printOn(Sortie& os) const
+{
+  os << "{"                  << finl;
+  os << "e_dry " << e_dry_   << finl;
+  os << "}"                  << finl;
+  return Fluide_Incompressible::printOn(os);
+}
+
+Entree& Solid_Particle::readOn(Entree& is)
+{
+  Fluide_Incompressible::readOn(is);
+  set_diameter_sphere(2*radius_sphere_);
+  set_volume_compo_sphere(4 * M_PI * pow(radius_sphere_, 3) / 3);
+  const double solid_density = masse_volumique().valeurs()(0, 0);
+  set_mass_compo_sphere(volume_compo_sphere_*solid_density);
+  return is;
+}
+
+void Solid_Particle::set_param(Param& param)
+{
+  Fluide_Incompressible::set_param(param);
+  param.ajouter("e_dry", &e_dry_,Param::REQUIRED); // XD_ADD_P dry coefficient;
+  param.ajouter("radius_sphere", &radius_sphere_); // XD_ADD_P radius of a spherical particle in the case of a sphere flow;
+  param.ajouter_non_std("spheroid", (this)); // XD_ADD_P definition of a revolution ellipsoid;
+}
+
+int Solid_Particle::lire_motcle_non_standard(const Motcle& word, Entree& is)
+{
+  if (word=="spheroid")
+    {
+      Motcles words;
+      words.add("half_small_axis");
+      words.add("half_long_axis");
+      Motcle secondword;
+      is >> secondword;
+      Motcle openbrace ="{";
+      Motcle closedbrace="}";
+      if (secondword==openbrace)
+        {
+          is >> secondword;
+          while (secondword != closedbrace)
+            {
+              int rang2 = words.search(secondword);
+              switch(rang2)
+                {
+                case 0:
+                  is >> half_small_axis_spheroid_;
+                  break;
+                case 1:
+                  is >> half_long_axis_spheroid_;
+                  break;
+                default:
+                  Cerr << "Solid_Particle::lire_motcle_non_standard\n"
+                       << " options of spheroid are:\n"
+                       << words;
+                  exit();
+                }
+              is >> secondword;
+            }
+        }
+      return 1;
+    }
+  else
+    {
+      Cerr << word << " is not a keyword understood by " << que_suis_je() << " in lire_motcle_non_standard"<< finl;
+      exit();
+    }
+  return -1;
+}
---
+++ src/Multiphase/fluid_particle_interaction/Solid_Particle.h
@@ -0,0 +1,70 @@
+/****************************************************************************
+* Copyright (c) 2025, CEA
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+* 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+* OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*****************************************************************************/
+
+#ifndef SOLID_PARTICLE_included
+#define SOLID_PARTICLE_included
+
+#include <Fluide_Incompressible.h>
+
+/*! @brief : class Solid_Particle
+ *
+ *  Description: This class describes spherical solid particles.
+ *  This class is used in the module fluid_particle_interaction.
+ *  Solid particles must be modeled with a high viscosity ratio
+ *  relative to the fluid.
+ *  suffix sphere :for sphere flow, therefore all particles have
+ *  the same physical properties.
+ *  The radius and e_dry coeff are used for the computation of the
+ *  solid-solid interactions. See Modele_Collision_FT
+ *  Location: ${TrioCFD_ROOT}/src/Multiphase/fluid_particle_interaction
+ */
+
+class Solid_Particle : public Fluide_Incompressible
+{
+  Declare_instanciable_sans_constructeur(Solid_Particle);
+
+public:
+  Solid_Particle();
+
+  // override functions
+  void set_param(Param& param) override;
+  int lire_motcle_non_standard(const Motcle&, Entree&) override;
+
+  // Setters
+  void set_radius(const double radius) { radius_sphere_=radius; }
+  void set_diameter_sphere(const double diameter) { diameter_sphere_=diameter; }
+  void set_volume_compo_sphere(const double volume) { volume_compo_sphere_=volume; }
+  void set_mass_compo_sphere(const double mass) { mass_compo_sphere_=mass; }
+
+  // getters
+  const double& get_e_dry() const { return e_dry_; }
+  const double& get_diameter_sphere() const { return diameter_sphere_; }
+  const double& get_volume_compo_sphere() const { return volume_compo_sphere_; }
+  const double& get_mass_compo_sphere() const { return mass_compo_sphere_; }
+  const double& get_half_small_axis_spheroid() const { return half_small_axis_spheroid_; }
+  const double& get_half_long_axis_spheroid() const { return half_long_axis_spheroid_ ;}
+
+protected :
+  // Variables declaration and initialization
+  double e_dry_=0.; // dry restitution coefficient
+  double radius_sphere_=0.; // only required for sphere flow
+  double diameter_sphere_=0.;
+  double volume_compo_sphere_=0.;
+  double mass_compo_sphere_=0.;
+  double half_small_axis_spheroid_=0;
+  double half_long_axis_spheroid_=0;
+};
+
+#endif
