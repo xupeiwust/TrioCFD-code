2c693c1e1e33c7343ed1eee1eb2145d64e9259b6
Teo BOUTIN <teo.boutin@cea.fr>
2025-06-17 19:00:55 +0200

    [doc] update PGC

--- share/doc_src/PGC/generer_PGC.sh
+++ share/doc_src/PGC/generer_PGC.sh
@@ -9,7 +9,7 @@ pdflatex ${DOC}.tex
 pdflatex ${DOC}.tex
 
 # Installation of the final pdf report
-#cp ${DOC}.pdf ../../doc
+cp ${DOC}.pdf ../../doc
 
 # Cleaning
 for ext in aux bbl blg idx lof log lot toc
