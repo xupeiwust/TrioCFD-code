c6f8fc6cf13e8d3de22eb512960ec5bc8b35fe72
Teo BOUTIN <teo.boutin@cea.fr>
2025-06-23 14:22:15 +0200

    [doc] Update model report

--- share/doc_src/model_report/generer_model_report.sh
+++ share/doc_src/model_report/generer_model_report.sh
@@ -13,7 +13,7 @@ pdflatex ${DOC}.tex
 pdflatex ${DOC}.tex
 
 # Installation of the final pdf report
-#cp ${DOC}.pdf ../../doc
+cp ${DOC}.pdf ../../doc
 
 # Cleaning
 for ext in aux bbl blg idx log lot toc
